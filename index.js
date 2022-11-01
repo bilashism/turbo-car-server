058b9260e358cef0670f57a84ae22004b35c6e2d
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-11-01 09:45:29 +0600

    removed unused vars

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const server = app.listen(port, () => console.log(`Running on: ${port}`));
+app.listen(port, () => console.log(`Running on: ${port}`));
 
 app.get("/", (req, res) => {
   res.sendStatus(200);
