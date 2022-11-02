da6c92dfc77cbc7ab9579b4591ce48ad013be29f
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-11-02 18:38:46 +0600

    service api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -17,11 +17,47 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1
 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+const run = async () => {
+  try {
+    const serviceCollection = client.db("turboCar").collection("services");
+
+    // get all services
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const options = {};
+
+      const cursor = serviceCollection.find(query, options);
+
+      const services = await cursor.toArray();
+
+      res.send(services);
+    });
+
+    // get single service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: ObjectId(id) };
+      const options = {};
+
+      const service = await serviceCollection.findOne(query, options);
+
+      res.send(service);
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+  }
+};
+
+run().catch(err => console.error(err));
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 app.listen(port, () => console.log(`Running on: ${port}`));
 
