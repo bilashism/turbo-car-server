fde974f4f85f01bbaa95a83a49467fff16886cb0
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-11-02 16:42:08 +0600

    basic node mongo setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,19 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@cluster0.3ackybm.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
+});
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.listen(port, () => console.log(`Running on: ${port}`));
 
